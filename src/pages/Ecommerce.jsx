1fa8bbe324934c93efd98ee2d44c0be6f01326e2
Aleksei Bezguzov <abezguzov@gmail.com>
2022-11-02 20:32:55 +0000

    Update package-lock.json and Ecommerce.jsx

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -32,6 +32,37 @@ const Ecommerce = () => {
             />
           </div>
         </div>
+        <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
+          {earningData.map((item) => (
+            <div
+              key={item.title}
+              className="bg-white dark:text-gray dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl"
+            >
+              <button
+                type="button"
+                style={{ color: item.iconColor, backgroundColor: item.iconBg }}
+                className="text-2xl rounded-full p-4 opacity-0.9 hover:drop-shadow-xl"
+              >
+                {item.icon}
+              </button>
+              <p className="mt-3">
+                <span className="text-lg font-semibold">{item.amount}</span>
+                <span className={`text-sm text-${item.pcColor} ml-2`}>
+                  {item.percentage}
+                </span>
+              </p>
+              <p className="text-sm text-gray-400 mt-1">{item.title}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="flex gap-10 flex-wrap justify-center">
+        <div className="bg-white dark:bg-secondary-dark-bg dark:text-gray-200 m-3 rounded-2xl md:w-700">
+          <div>
+            <p>Revenue updates</p>
+          </div>
+        </div>
       </div>
     </div>
   );

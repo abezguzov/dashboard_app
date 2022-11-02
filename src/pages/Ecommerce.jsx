ee10715046ea617c0255e32453f84ceafa302447
AB <abezguzov@gmail.com>
2022-11-02 21:01:10 +0100

    Initial commit

---
+++ src/pages/Ecommerce.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { BsCurrencyDollar } from "react-icons/bs";
+import { GoPrimitiveDot } from "react-icons/go";
+import { Stacked, Pie, Button, SparkLine } from "../components";
+import {
+  earningData,
+  SparklineAreaData,
+  ecomPieChartData,
+} from "../data/dummy";
+import { useStateContext } from "../contexts/ContextProvider";
+
+const Ecommerce = () => {
+  return (
+    <div className="mt-12">
+      <div className="flex flex-wrap lg:flex-nowrap justify-center">
+        {/* DIV holding banner */}
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repat bg-cover bg-center">
+          {/* DIV holding Earnings */}
+          <div className="flex justify-between items-center ">
+            <div>
+              <p className="font-bold text-gray-300">Earnings</p>
+              <p className="text-2xl text-gray-300">$7,900</p>
+            </div>
+          </div>
+          <div className="mt-6">
+            <Button
+              color="white"
+              bgColor="blue"
+              text="Download"
+              borderRadius="10px"
+              size="md"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Ecommerce;
---
+++ tailwind.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
+  theme: {
+    fontFamily: {
+      display: ["Open Sans", "sans-serif"],
+      body: ["Open Sans", "sans-serif"],
+    },
+    extend: {
+      fontSize: {
+        14: "14px",
+      },
+      backgroundColor: {
+        "main-bg": "#FAFBFB",
+        "main-dark-bg": "#20232A",
+        "secondary-dark-bg": "#33373E",
+        "light-gray": "#F7F7F7",
+        "half-transparent": "rgba(0, 0, 0, 0.5)",
+      },
+      borderWidth: {
+        1: "1px",
+      },
+      borderColor: {
+        color: "rgba(0, 0, 0, 0.1)",
+      },
+      width: {
+        400: "400px",
+        760: "760px",
+        780: "780px",
+        800: "800px",
+        1000: "1000px",
+        1200: "1200px",
+        1400: "1400px",
+      },
+      height: {
+        80: "80px",
+      },
+      minHeight: {
+        590: "590px",
+      },
+      backgroundImage: {
+        "hero-pattern":
+          "url('https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80')",
+      },
+    },
+  },
+  plugins: [],
+};
